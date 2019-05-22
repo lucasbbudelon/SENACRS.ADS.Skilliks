80e3afdd795d5a32000db87e8fd92e746277da9e
Lucas B Budelon <lucasbbudelon@gmail.com>
2019-05-21 21:25:08 -0300

    Listagem da vagas e tela de detalhes

--- skilliks-frontend/src/app/components/sidebar/sidebar.component.ts
+++ skilliks-frontend/src/app/components/sidebar/sidebar.component.ts
@@ -7,16 +7,11 @@ declare interface RouteInfo {
   icon: string;
   class: string;
 }
+
 export const ROUTES: RouteInfo[] = [
   { path: '/usuarios', title: 'Usuários', icon: 'fa-users', class: 'text-primary' },
-  { path: '/candidaturas', title: 'Candidaturas', icon: 'fa-chalkboard-teacher', class: 'text-primary' }
-  // { path: '/dashboard', title: 'Dashboard', icon: 'ni-tv-2 text-primary', class: '' },
-  // { path: '/icons', title: 'Icons', icon: 'ni-planet text-blue', class: '' },
-  // { path: '/maps', title: 'Maps', icon: 'ni-pin-3 text-orange', class: '' },
-  // { path: '/user-profile', title: 'User profile', icon: 'ni-single-02 text-yellow', class: '' },
-  // { path: '/tables', title: 'Tables', icon: 'ni-bullet-list-67 text-red', class: '' },
-  // { path: '/login', title: 'Login', icon: 'ni-key-25 text-info', class: '' },
-  // { path: '/register', title: 'Register', icon: 'ni-circle-08 text-pink', class: '' }
+  { path: '/candidaturas', title: 'Candidaturas', icon: 'fa-chalkboard-teacher', class: 'text-primary' },
+  { path: '/vagas', title: 'Vagas', icon: 'fa-user-ti', class: 'text-primary' }
 ];
 
 @Component({
